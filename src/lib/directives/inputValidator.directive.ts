23c61b6e4cc316f6c45c3ec623bf7a2e48274aa9
Jhonatan <jhoonatan_0912@hotmail.com>
2024-04-25 11:56:34 -0500

    feat: Update inputValidatorDirective and upgrade version

--- src/lib/directives/inputValidator.directive.ts
+++ src/lib/directives/inputValidator.directive.ts
@@ -1,4 +1,6 @@
-import { Directive, HostListener, Input } from '@angular/core';
+import { Directive, EventEmitter, HostListener, Input, Output } from '@angular/core';
+
+type InputType = 'email' | 'password' | 'text' | 'number' | 'tel' | 'url' | 'search' | 'textarea';
 
 @Directive({
   selector: '[inputValidator]',
@@ -6,27 +8,53 @@ import { Directive, HostListener, Input } from '@angular/core';
 })
 export class InputValidatorDirective {
 
-  @Input() inputType!: 'number' | 'phone' | 'email' | 'text';
+  @Input() inputType!: InputType;
+  @Output() isValid = new EventEmitter<boolean>();
 
-  private readonly patterns = {
-    number: /^[0-9]*$/,
+  private readonly patterns: Record<InputType, RegExp> = {
+    email: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+    password: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,}$/,
     text: /^[a-zA-Z\s]*$/,
-    phone: /^[0-9]*$/,
-    email: /^[a-zA-Z0-9@.]*$/
+    number: /^\d+$/,
+    tel: /^\d{10}$/,
+    url: /^https:\/\/.*/,
+    search: /^.{3,}$/,
+    textarea: /^.{10,}$/,
   };
 
-  @HostListener('input', ['$event'])
-  onInput(event: any): void {
-    const input = event.target as HTMLInputElement;
-    const value = input.value;
-    const pattern = this.getPattern();
-    const isValid = pattern.test(value);
-    if (!isValid) {
-      input.value = value.slice(0, -1);
+  @HostListener('keyup', ['$event']) onInput(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    const isValid = this.validateInput(target.value.trim());
+    this.updateValidityStyle(target, isValid);
+    this.isValid.emit(isValid);
+  }
+
+  private validateInput(value: string): boolean {
+    const pattern = this.patterns[this.inputType];
+    switch (this.inputType) {
+      case 'email':
+        return pattern.test(value);
+      case 'password':
+        return pattern.test(value);
+      case 'text':
+        return value.length >= 3;
+      case 'number':
+        return !isNaN(Number(value));
+      case 'tel':
+        return value.length === 10;
+      case 'url':
+        return value.includes('https://');
+      case 'search':
+        return value.length >= 3;
+      case 'textarea':
+        return value.length >= 10;
+      default:
+        return false;
     }
   }
 
-  private getPattern(): RegExp {
-    return this.patterns[this.inputType] || /^[a-zA-Z0-9\s]*$/;
+  private updateValidityStyle(target: HTMLInputElement, isValid: boolean): void {
+    target.style.borderColor = isValid ? 'green' : 'red';
+    target.style.borderWidth = isValid ? '2px' : '1.5px';
   }
 }
